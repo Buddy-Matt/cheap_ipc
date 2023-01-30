df5780b81aaf4bcd9b52f997d9a2068aecc14ef5
Matt Gray <Matt@Matt-Gray.co.uk>
2023-01-30 00:44:22 +0000

    Working build

--- build-v4lrs.sh
+++ build-v4lrs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd 3rdPartyTools
-rm v4l2rtspserver -r
-git clone git@github.com:mpromonet/v4l2rtspserver.git
-cd v4l2rtspserver
-cmake -DLIVE555CFLAGS="-DNEWLOCALE_NOT_USED=1  -DBSD=1 -DSOCKLEN_T=socklen_t -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE=1 -DALLOW_RTSP_SERVER_PORT_REUSE=1" .
+cd 3rdPartyTools/v4l2rtspserver
+git apply ../v4l2rs_removeH264.patch
+
+cmake -DLIVE555CFLAGS="-DNEWLOCALE_NOT_USED=1; -DBSD=1; -DSOCKLEN_T=socklen_t; -D_FILE_OFFSET_BITS=64; -D_LARGEFILE_SOURCE=1; -DALLOW_RTSP_SERVER_PORT_REUSE=1; -DNO_OPENSSL=1;" .
 make
