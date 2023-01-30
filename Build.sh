933e2e194c83a86aaff2f8b8c67cbf713a5c0264
Matt Gray <Matt@Matt-Gray.co.uk>
2023-01-30 22:01:46 +0000

    New submodule and improved build

--- Build.sh
+++ Build.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
-echo "Checking out submodules"
-
+echo "*************************"
+echo "*Checking out submodules*"
+echo "*************************"
+echo ""
 git submodule update --init
 
+echo ""
+echo "********************************************"
+echo "*Prepping dockcross for cross-env compiling*"
+echo "********************************************"
+echo ""
 #create dockcross cmd here
 cd 3rdPartyTools/dockcross/
 docker run --rm dockcross/linux-armv5-uclibc > ../../dockcross-run
 cd ../../
 chmod +x dockcross-run
 
-echo "Compiling v4lrtspserver"
+
+echo ""
+echo "*************************"
+echo "*Compiling v4lrtspserver*"
+echo "*************************"
+echo ""
 ./dockcross-run ./build-v4lrs.sh
 
+
+echo ""
+echo "************************"
+echo "*Creating SD Card image*"
+echo "************************"
+echo ""
 
 mkdir -p SD_Card/Factory
 mkdir -p SD_Card/bin
 mkdir -p SD_Card/www
 
 #main script to intercept and stop the normal boot stuff
-cp src/scripts/config.sh SD_Card/Factory/
+cp -v src/scripts/config.sh SD_Card/Factory/
 #webserver
-cp 3rdPartyTools/zsgx1hacks/sdcard/hack/busybox-armv6l SD_Card/bin/busybox
-cp -r src/webserver SD_Card/www
+cp -v 3rdPartyTools/zsgx1hacks/sdcard/hack/busybox-armv6l SD_Card/bin/busybox
+cp -rv src/webserver SD_Card/www
 
 #v4l2rtspserver
-cp 3rdPartyTools/v4l2rtspserver/v4l2rtspserver SD_Card/bin
+cp -v 3rdPartyTools/v4l2rtspserver/v4l2rtspserver SD_Card/bin
+
+echo ""
+echo "****************"
+echo "*Build Complete*"
+echo "****************"
+echo ""
+echo "Copy the contents of SD_Card to the root of a suitable SD Card, insert it in the camera & power on"
+echo ""
--- src/scripts/config.sh
+++ src/scripts/config.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
+logger Custom Firmware Mode
 
-logger Factory Mode
-
+#start a telnet daemon
 busybox telnetd -p 23 -l /bin/sh &
 
+#bring up the network
 ifconfig eth0 up
 sleep 1
 
-#This is nromally commented out. It starts the wifi. Lets have it back :)
+#This is normally commented out in the "official" firmware. But it starts the wifi... Lets have it back :)
 /usr/sbin/net_manage.sh &
 
-#This pisses around with things like unmounting the SD card
-#daemon
+#start a lightweight http server that can do stuff like turn the lights on & off
+/mnt/bin/busybox httpd -h /mnt/www/
 
-#This actualy runs the camaera server. It talks to base. Lets leave it alone 
-#/usr/sbin/anyka_ipc.sh start
-
-/mnt/hack/busybox httpd -h /mnt/hack/www/
-
-#But we owuld like to know the time please
+#Sort out the timezone using the "offical" method
 source /etc/jffs2/time_zone.sh
 
+#Wait for network connectivity
 while ! ping -c 1 -W 1 192.168.13.254; do
     echo "Waiting for 1.2.3.4 - network interface might be down..."
     sleep 1
 done
 
+#Set the time
 logger setting time
 ntpd -N -p uk.pool.ntp.org
